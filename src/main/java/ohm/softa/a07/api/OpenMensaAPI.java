2743571a0cddca156afca4f926ba88b32580b5a4
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-05-02 11:07:31 +0200

    workaround for ant being a pile of fail

--- src/main/java/ohm/softa/a07/api/OpenMensaAPI.java
+++ src/main/java/ohm/softa/a07/api/OpenMensaAPI.java
@@ -1,10 +1,28 @@
 package ohm.softa.a07.api;
 
+import ohm.softa.a07.model.Meal;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+import java.util.List;
+
 /**
  * Created by Peter Kurfer on 11/19/17.
  */
 
 public interface OpenMensaAPI {
-	// TODO add method to get meals of a day
 	// example request: GET /canteens/229/days/2017-11-22/meals
+
+	/**
+	 * example request: GET /canteens/269/days/2023-05-02/meals
+	 * @param canteenId canteenId für Nuremberg is 269
+	 * @param date Date in format yyyy-MM-dd (ISO-standard)
+	 * @return List of meals for that day
+	 */
+	@GET ("/canteens/{canteen_id}/days/{date}/meals")
+	Call<List<Meal>> getMealsByDate(
+		@Path("canteen_id") int canteenId,
+		@Path("date") String date);
+
 }
--- src/main/java/ohm/softa/a07/controllers/MainController.java
+++ src/main/java/ohm/softa/a07/controllers/MainController.java
@@ -8,9 +8,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import ohm.softa.a07.api.OpenMensaAPI;
+import ohm.softa.a07.model.Meal;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
+import java.io.IOException;
 import java.net.URL;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 
 public class MainController implements Initializable {
 
@@ -21,6 +32,12 @@ public class MainController implements Initializable {
 	@FXML
 	private ListView<String> mealsList;
 
+	private OpenMensaAPI api = new Retrofit.Builder()
+		.addConverterFactory(GsonConverterFactory.create())
+		.baseUrl("https://openmensa.org/api/v2/")
+		.build()
+		.create(OpenMensaAPI.class);
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// set the event handler (callback)
@@ -28,9 +45,29 @@ public class MainController implements Initializable {
 			@Override
 			public void handle(ActionEvent event) {
 				// create a new (observable) list and tie it to the view
-				ObservableList<String> list = FXCollections.observableArrayList("Hans", "Dampf");
+				ObservableList<String> list = FXCollections
+					.observableArrayList(getMeals());
 				mealsList.setItems(list);
 			}
 		});
+	}
+
+	private List<String> getMeals() {
+		List<Meal> response = null;
+		try {
+			response = api.getMealsByDate(
+					229, LocalDate.now().toString())
+				.execute().body();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		if (response == null) {
+			return Collections.emptyList();
+		}
+
+		return response.stream()
+			.map(Meal::toString)
+			.collect(Collectors.toList());
 	}
 }
